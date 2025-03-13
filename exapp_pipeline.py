8e1cbf98cb0fbcd8d9ca96891f911a896c9168a2
nacht29 <nacht29.studygmail.com>
2025-03-13 13:16:23 +0800

    feat: import modules

---
+++ exapp_pipeline.py
@@ -0,0 +1,16 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.models.param import Param
+from airflow.utils.log.logging_mixin import LoggingMixin
+
+import os
+import warnings
+import subprocess
+import pandas as pd
+from datetime import date, datetime
+from google.cloud import bigquery as bq
+from google.cloud import storage
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from google.api_core.exceptions import Forbidden, NotFound
