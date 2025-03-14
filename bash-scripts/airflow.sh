9a944f1aa3f041d90c6f5221db90682988629604
nacht29 <nacht29.studygmail.com>
2025-03-14 10:43:05 +0800

    feat: added function to load files to Google Drive
    - function first searches if Year and Moth folder exists in drive
    - create (if Year folder does not exists) and return correspondind Year folder id
    - create and return Month folder id
    - Month folder id is used as parent id for uploading CSV files

--- bash-scripts/airflow.sh
+++ bash-scripts/airflow.sh
@@ -1,3 +1,2 @@
-cd ..
 tmux new-session -d -s airflow_web "airflow db init && airflow webserver -p 8080"
 tmux new-session -d -s airflow_sched "airflow scheduler"
--- exapp_pipeline.py
+++ exapp_pipeline.py
@@ -30,14 +30,16 @@ from google.api_core.exceptions import Forbidden, NotFound
 TIME_ZONE = pendulum.timezone('Asia/Singapore')
 START_DATE = datetime(2025, 3, 11, tzinfo=TIME_ZONE)
 
-JSON_KEYS_PATH = '/home/yanzhe/gchexapp01p/json-keys/gch-prod-dwh01-data-pipeline.json'
+JSON_KEYS_PATH = 'json-keys/gch-prod-dwh01-data-pipeline.json'
+# JSON_KEYS_PATH = '/home/yanzhe/gchexapp01p/json-keys/gch-prod-dwh01-data-pipeline.json'
 SERVICE_ACCOUNT = f'{JSON_KEYS_PATH}'
 
 # Google Drive params
 SCOPES = ['https://www.googleapis.com/auth/drive']
-PARENT_FOLDER_ID = '1GJ2pAchszF5MWFIV7vNoMyv76xa3Wvbm' # Possales_RL > Hyper
+MAIN_PARENT_FOLDER_ID = '1GJ2pAchszF5MWFIV7vNoMyv76xa3Wvbm' # Possales_RL > Hyper
 
-SQL_SCRIPTS_PATH = '/home/yanzhe/gchexapp01p/sql-scripts/sc-possalesrl/'
+SQL_SCRIPTS_PATH = 'sql-scripts/sc-possalesrl/'
+# SQL_SCRIPTS_PATH = '/home/yanzhe/gchexapp01p/sql-scripts/sc-possalesrl/'
 
 # 1000000
 SLICE_BY_ROWS = 100
@@ -63,7 +65,7 @@ def gen_file_name(infile_name:str, infile_type:str, outfile_type:str, ver:int):
 	return file_name
 
 # return (mm,yyyy)
-def gen_date() -> tuple:
+def get_month_year() -> tuple:
 	month = calendar.month_name[datetime.now().month]
 	year = datetime.now().year
 
@@ -89,8 +91,8 @@ def query_data():
 				# upload subset as csv
 				subset.to_csv(f'{out_filename}', sep='|', encoding='utf-8', index=False, header=True)
 
-def gen_bucket_path():
-	month, year = gen_date()
+def filepath_in_bucket():
+	month, year = get_month_year()
 	return f'supply_chain/possales_rl/{year}/{month}'
 
 def load_bucket():
@@ -98,65 +100,117 @@ def load_bucket():
 
 	load_files = file_type_in_dir(None, '.csv')
 	for file in load_files:
-		path_in_bucket = f'{gen_bucket_path()}/{file}'
+		path_in_bucket = f'{filepath_in_bucket()}/{file}'
 		# bucket.blob(path_in_bucket).upload()
 		blob = bucket.blob(path_in_bucket)
 		blob.upload_from_filename(file)
+
+def drive_autodetect_folders(service, parent_folder_id:str, folder_name:str):
+	'''
+	# searches if folder exists in drive
+	# returns a list of dict
+	# id = file/folder id
+	# name = file/folder name
+	files = [
+		{'id':0, 'name':'A'},
+		{'id':1, 'name':'B'}
+	]
+	'''
+
+	query = f"""
+	'{parent_folder_id}' in parents 
+	and name='{folder_name}'
+	and mimeType='application/vnd.google-apps.folder' 
+	and trashed=false
+	"""
+
+	results = service.files().list(q=query, fields='files(id,name)').execute()
+	files_in_drive = results.get('files') # files_in_drive = results.get('files', [])
+
+	if files_in_drive:
+		return files_in_drive[0]['id']
+	else:
+		file_metadata = {
+			'name': folder_name,
+			'mimeType': 'application/vnd.google-apps.folder',
+			'parents': [parent_folder_id]
+		}
+
+		folder = service.files().create(
+			body=file_metadata,
+			fields='id'
+		).execute()
+
+		return folder['id']
 
 def load_gdrive():
 	# authenticate
 	creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT, scopes=SCOPES)
 	service = build('drive', 'v3', credentials=creds)
+	month, year = get_month_year()
 
-	# get all existing files in Drive
-	query = f"'{PARENT_FOLDER_ID}' in parents and trashed=false"
-	response = service.files().list(q=query, fields='files(id, name)').execute()
+	# auto detect folders - create folder if destination folder does not exists
+	year_folder_id = drive_autodetect_folders(service, MAIN_PARENT_FOLDER_ID, year)
+	month_folder_id = drive_autodetect_folders(service, year_folder_id, month)
 
 	# get name of all files to be loaded
-	load_files = file_type_in_dir(None, '.csv')
+	csv_files = file_type_in_dir(None, '.csv')
 
-	# check for duplicates
-	for load_file in load_files:
-		# get list of dup files
-		query = f"'{PARENT_FOLDER_ID}' in parents and name='{load_file}' and trashed=false"
-		response = service.files().list(q=query, fields='files(id, name)').execute()
-		dup_files = response.get('files')
+	for csv_file in csv_files:
+		query = f"""
+		'{month_folder_id}' in parents
+		and name = '{csv_file}'
+		and trashed=false
+		"""
 
-		# delete duplicates if any
+		results = service.files().list(q=query, fields='files(id, name)').execute()
+		dup_files = results.get('files')
+
 		if dup_files:
 			for dup_file in dup_files:
-				service.files().delete(fileId=dup_file['id']).execute()
+				service.files().delete(fileID=dup_file['id']).execute()
+
+		file_metadata = {
+			'name':csv_file,
+			'parents': [month_folder_id]
+		}
+
+		file = service.files().create(
+			body=file_metadata,
+			media_body=csv_file
+		).execute()
 
 def remove_outfiles():
-	outfiles = file_type_in_dir(None, '.csv')
-	for outfile in outfiles:
-		os.remove(outfile)
+	csv_files = file_type_in_dir(None, '.csv')
+	for csv_file in csv_files:
+		os.remove(csv_file)
 
-# query_data()
-# load_bucket()
-# remove_outfiles()
+query_data()
+load_bucket()
+load_gdrive()
+remove_outfiles()
 
-with DAG(
-	'exapp_pipeline',
-	start_date=START_DATE,
-	schedule="00 07 * * *",
-	catchup=True
-) as dag:
+# with DAG(
+# 	'exapp_pipeline',
+# 	start_date=START_DATE,
+# 	schedule="45 00 * * *",
+# 	catchup=True
+# ) as dag:
 	
-	task_query_data = PythonOperator(
-		task_id='query_data',
-		python_callable=query_data,
-	)
+# 	task_query_data = PythonOperator(
+# 		task_id='query_data',
+# 		python_callable=query_data,
+# 	)
 
-	task_load_bucket = PythonOperator(
-		task_id='load_bucket',
-		python_callable=load_bucket
-	)
+# 	task_load_bucket = PythonOperator(
+# 		task_id='load_bucket',
+# 		python_callable=load_bucket
+# 	)
 
-	task_remove_outfiles = PythonOperator(
-		task_id='remove_outfiles',
-		python_callable=remove_outfiles
-	)
+# 	task_remove_outfiles = PythonOperator(
+# 		task_id='remove_outfiles',
+# 		python_callable=remove_outfiles
+# 	)
 	
-	task_query_data >> [task_load_bucket]
-	task_load_bucket >> task_remove_outfiles
+# 	task_query_data >> [task_load_bucket]
+# 	task_load_bucket >> task_remove_outfiles
