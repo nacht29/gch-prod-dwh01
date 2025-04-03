7ed4087e4026243e07c787dadd52636bac5ac1f8
nacht29 <nacht29.studygmail.com>
2025-04-03 17:53:57 +0800

    feat: refined mail feature

--- landlord_report/alert.py
+++ landlord_report/alert.py
@@ -1,13 +1,31 @@
 import sys
 from pygcp.utils import send_email
 
+subject = '[ALERT - WARNING] [GCH] Pipeline Failed'
+
+message = f"""
+Pipeline: landlord_report_pipeline failed to execute.
+File export is incomplete.
+
+Error message:
+
+{sys.argv[1]}
+
+Try:
+1. Check file paths defined in the Python script
+2. Check for dependencies (import xyz)
+3. Check for syntax errors (missing symbols, double single quote (' '' '))
+
+Contact Data Team for further checking.
+"""
+
 send_email(
-	subject='Alert: landlord_report_pipeline_dev failed',
-	message=sys.argv[1],
+	subject=subject,
+	message=message,
 	smtp_server='smtp.gmail.com',
 	smtp_port=587,
 	sender_email='yanzhe@cloud-space.co',
 	app_password='isss lyjp jyni wfej',
 	receiver_email='wilken@cloud-space.co',
-	cc_emails=['amanda@cloud-space.co']
+	cc_emails=['amanda@cloud-space.co', 'yanzhe@cloud-space.co']
 )
