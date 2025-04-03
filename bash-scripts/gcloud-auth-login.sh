bd7bff3cc0e465ed92ad13a1f1c2c0048a4a12bf
nacht29 <nacht29.studygmail.com>
2025-04-03 16:18:52 +0800

    refactor: restruct files

--- bash-scripts/gcloud-auth-login.sh
+++
@@ -1,9 +0,0 @@
-gcloud auth login
-
-gcloud auth list
-
-gcloud config set project <PROJECT ID>
-
-gcloud config list
-
-gcloud compute start-iap-tunnel gchexpapp01p 8080     --local-host-port=localhost:3389     --zone=asia-southeast1-a
--- bash-scripts/git-pull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set PATH fro commands
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-# git pull in gwh-prod-dwh01
-cd /home/yanzhe/gch-prod-dwh01/ || exit 1
-/usr/bin/git pull
